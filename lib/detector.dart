2f343c38bd8dc14c429f8736789c31dae14591f1
Alexander Oroudjev <a.oroudjev@gmail.com>
2024-04-03 01:26:50 -0700

    Add box extraction and drawing

--- lib/detector.dart
+++ lib/detector.dart
@@ -35,13 +35,9 @@ class Detector {
     await Interpreter.fromAsset(_modelPath, options: interpreterOptions);
   }
 
-  Uint8List? analyzeImage(imageData) {
+  Uint8List? analyzeImage(img.Image image) {
     //Analyze image and run inference with a returned image with bounding boxes
-    final image = img.decodeImage(imageData);
-
-    if (image == null) {
-      throw Exception("Failed to decode image.");
-    }
+    // Model requires 320x320 sized image
 
     final imageInput = img.copyResize(image, width: 320, height: 320);
 
@@ -57,6 +53,45 @@ class Detector {
     );
 
     final output = _runInference(imageMatrix);
+
+    final scoresTensor = output[0].first as List<double>;
+    final boxesTensor = output[1].first as List<List<double>>;
+
+    log('Processing outputs...');
+
+    final List<List<int>> locations = boxesTensor
+        .map((box) => box.map((value) => ((value * 300).toInt())).toList())
+        .toList();
+    final numberOfDetections = output[2].first as double;
+
+    log('Outlining objects...');
+
+    for (var i = 0; i < numberOfDetections; i++) {
+      if (scoresTensor[i] > 0.85) {
+        // Object
+        img.drawRect(
+          imageInput,
+          x1: locations[i][1],
+          y1: locations[i][0],
+          x2: locations[i][3],
+          y2: locations[i][2],
+          color: img.ColorRgb8(0, 255, 0),
+          thickness: 3,
+        );
+
+        // Label
+        img.drawString(
+          imageInput,
+          'palm: ${scoresTensor[i]}',
+          font: img.arial14,
+          x: locations[i][1] + 7,
+          y: locations[i][0] + 7,
+          color: img.ColorRgb8(0, 255, 0),
+        );
+      }
+    }
+
+    log('Done.');
     return img.encodeJpg(imageInput);
   }
 
@@ -65,14 +100,13 @@ class Detector {
       ) {
     log('Running inference...');
 
-    // Set input tensor [1, 300, 300, 3]
     final input = [imageMatrix];
 
     // Set output tensor
     // Scores: [1, 10],
     // Locations: [1, 10, 4],
     // Number of detections: [1],
-    // Classes: [1, 10],
+    // Classes: [1, 10] *not included as we do not use them*,
     final output = {
       0: [List<num>.filled(10, 0)],
       1: [List<List<num>>.filled(10, List<num>.filled(4, 0))],
